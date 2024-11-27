c53d75ff5e3b6103cfe05583bc65d952a2ac1396
Greeshbiju <bijugreeshma64@gmail.com>
2024-11-27 16:08:20 +0530

    Add files via upload

--- queue.c
+++ queue.c
@@ -1,79 +1,83 @@
-#include<stdio.h>
-#include<stdlib.h>
-#define max 50
-int queue[max],front=-1,rear=-1;
-void enqueue()
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 50 
+int queue[MAX], front = -1, rear = -1; 
+void enqueue() 
 {
-int elem;
-if(rear==max-1)
+	int elem;
+	if (rear == MAX - 1) 
+	{
+		printf("Queue Overflow! \n");
+		return;
+	}
+	printf("Enter the element to be enqueued: ");
+	scanf("%d", &elem);
+	if (front == -1) 
+	{
+		front = 0; 
+	}
+	rear++; 
+	queue[rear] = elem; 
+	printf("%d enqueued to the queue.\n", elem);
+}
+
+void dequeue() 
 {
-printf("Queue overflow/");
-return 0;
+	if (front == -1 || front > rear) 
+	{
+		printf("Queue Underflow! \n");
+		return;
+	}
+	printf("Dequeued element: %d\n", queue[front]);
+	front++; 
+	if (front > rear)
+	{
+		front = rear = -1;
+	}
 }
-printf("Enter the elements to be enqueued:");
-scanf("%d",& elem);
-if(frpnt==-1)
-front=0;
-}
-rear++;
-queue[rear]=elem;
-printf("%d enqueued to the queue/n",elem);
-}
-void dequeue()
-{
-if(front==-1// front=rear)
-{
-printf('Queue underflow"/");
-return;
-}
-printf("dequeued element:%d/n",queue[front]);
-front++;
-if(front>rear)
-{
-front=rear=-1;
-}
-}
+
 void display()
 {
-int i;
-if(front==-1)
+	int i;
+	if (front == -1) 
+	{
+		printf("Queue is empty.\n");
+		return;
+	}
+	printf("Queue elements are:\n");
+	for (i = front; i <= rear; i++) 
+	{
+		printf("%d ", queue[i]);
+	}
+	printf("\n");
+}
+int main() 
 {
-printf("queue is empty/n");
-return;
-}
-printf("Queue elements are:/n");
-for[i=front;1<=rear;i++);
-{
-printf("%d",Queue[1]);
-}
-print("/n");
-}
-print("/n");
-}
-int main()
-{
-int ch;
-while(1)
-{
-print("/n queue menu/n');
-printf("1.enqueue/n");
-printf("2.dequeue/n");
-printf("3.display/n"");
-printf("4.exit/n");
-printf("Enter ypur choice:);
-scanf("%d",&ch));
-switch(ch)
-{
-case 1:enueueue();
-break;
-case 2:dequeue();
-break;
-case 3:display ();
-break;
-case 4:exit(0);
-default :printf("Invalid choice;try again/n");
-}
-}
-return 0;
+	int ch;
+	while (1)
+	{
+		printf("\n--- Queue Menu ---\n");
+		printf("1. Enqueue\n");
+		printf("2. Dequeue\n");
+		printf("3. Display\n");
+		printf("4. Exit\n");
+		printf("Enter your choice: ");
+		scanf("%d", &ch);
+		switch (ch) 
+		{
+			case 1:enqueue();
+				break;
+			case 2:dequeue();
+				break;
+			case 3:display();
+				break;
+			case 4:exit(0);
+			default:
+				printf("Invalid choice, please try again.\n");
+		}
+	}
+	return 0;
 }
 
